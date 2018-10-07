28fc4248570659198b5cb21ac998f798da73a76c
MatthewDodi <matthew.dodi@gmail.com>
2018-10-06 22:00:33 -0700

    set up of app.js

--- app.js
+++ app.js
@@ -4,3 +4,28 @@ const ejs = require('ejs');
 const Nexmo = require('nexmo');
 const socketio = require('socket.io');
 
+// Initializing app
+
+const app = express();
+
+// Template engine setup
+app.set('view engine', 'html');
+app.engine('html', ejs.renderFile);
+
+// Public folder setup
+app.use(express.static(__dirname + '/public'));
+
+// Body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Index route
+app.get('/', (req, res) => {
+    res.render('index');
+})
+
+// Define port
+const port = 3000;
+
+// Start server
+const server = app.listen(port, () => console.log(`Server started on port ${port}`));
